a49d0cb2068650dd7e7301309d034e5464bcf074
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2018-10-19 18:44:22 +0700

    Add some custom shortcodes

--- themes/enfold-child-2/functions.php
+++ themes/enfold-child-2/functions.php
@@ -5,7 +5,29 @@
 */
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
+namespace {
 
 add_theme_support( 'avia_custom_shop_page' );
+
+}
+
+
+namespace rotr {
+
+function show_product( $atts )
+{
+	$p = new \WC_Product( $atts['id'] );
+	return '<a href="'.$p->get_permalink().'"">'.$p->get_image().'</a>';
+}
+add_shortcode( 'rr_product', __NAMESPACE__.'\show_product' );
+
+function show_category( $atts )
+{
+    $img = wp_get_attachment_image_src( get_woocommerce_term_meta( $atts['id'], 'thumbnail_id' ), 'shop_catalog' )[0];
+	return '<a href="'.get_term_link((int)$atts['id']).'""><img src="'.$img.'"></a>';
+}
+add_shortcode( 'rr_category', __NAMESPACE__.'\show_category' );
+
+}
 
 // END ENQUEUE PARENT ACTION
