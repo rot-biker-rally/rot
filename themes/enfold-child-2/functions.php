040013dd1ad8c4276f6ee2cb7e10ee7f71be736e
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2018-10-19 16:13:08 +0700

    Make Enfold & WooCommerce be friends

--- themes/enfold-child-2/functions.php
+++ themes/enfold-child-2/functions.php
@@ -1,10 +1,11 @@
 <?php
-
 /*
-* Add your own functions here. You can also copy some of the theme functions into this file. 
+* Add your own functions here. You can also copy some of the theme functions into this file.
 * Wordpress will use those functions instead of the original functions then.
 */
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+add_theme_support( 'avia_custom_shop_page' );
 
 // END ENQUEUE PARENT ACTION
