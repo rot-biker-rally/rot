7228f5c6fb10381ff7871fd4980685c8f9f36e03
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2018-10-20 20:59:00 +0700

    Checkout shift coupon input location

--- themes/enfold-child-2/functions.php
+++ themes/enfold-child-2/functions.php
@@ -28,6 +28,18 @@ function show_category( $atts )
 }
 add_shortcode( 'rr_category', __NAMESPACE__.'\show_category' );
 
+function checkout_coupon_yank()
+{
+    remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
 }
+add_action( 'woocommerce_before_checkout_form', __NAMESPACE__.'\checkout_coupon_yank', 9 );
+
+function checkout_coupon_stuff()
+{
+    add_action( 'woocommerce_checkout_after_customer_details', 'woocommerce_checkout_coupon_form', 10 );
+}
+add_action( 'woocommerce_checkout_after_customer_details', __NAMESPACE__.'\checkout_coupon_stuff', 9 );
+
+} //namespace rotr
 
 // END ENQUEUE PARENT ACTION
