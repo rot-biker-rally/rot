912c23e43c6f6ebe7362ab25a57040b4514a1023
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2018-11-14 12:15:49 +0700

    Set up JS pipeline & enqueueing; general build cleanup

--- themes/enfold-child-2/functions.php
+++ themes/enfold-child-2/functions.php
@@ -8,4 +8,16 @@
 
 add_theme_support( 'avia_custom_shop_page' );
 
+function enqueue() {
+	if ( is_admin() ) { return; }
+
+	$js_deps = array( 'jquery' );
+
+	if( is_tax('product_cat', 'rv-spaces' ) ) {
+		wp_enqueue_script( 'map-index', get_stylesheet_directory_uri() . '/dist/js/map-index.js', false, true );
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue', 999 );
+
 // END ENQUEUE PARENT ACTION
