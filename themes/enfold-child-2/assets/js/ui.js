34f0a2600dedb52ba4d56658479a366b32ba9025
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2018-11-16 11:55:57 +0700

    Single ticket UX adjustments

--- themes/enfold-child-2/assets/js/ui.js
+++ themes/enfold-child-2/assets/js/ui.js
@@ -1,14 +1,31 @@
 jQuery(document).ready($ => {
 
-const $nameField = $('.product_cat-rv-spaces .bundled_item_cart_details .product-addon-attendee p.form-row')
-const $shirtField = $('.product_cat-rv-spaces .bundled_item_cart_details .product-addon-shirt-size select option:first-child')
+//////////////
+// RV Space //
+//////////////
 
-$nameField.each( (idx, el) => {
+const $nameFieldRV = $('.product_cat-rv-spaces .bundled_item_cart_details .product-addon-attendee p.form-row')
+const $shirtFieldRV = $('.product_cat-rv-spaces .bundled_item_cart_details .product-addon-shirt-size select option:first-child')
+
+$nameFieldRV.each( (idx, el) => {
 	const $el = $(el)
 	const label = $el.children('label').text()
 	$el.children('input').attr('placeholder', label.trim());
 })
 
-$shirtField.prop('disabled', true).text('Select shirt size...')
+$shirtFieldRV.prop('disabled', true).text('Select shirt size...')
+
+////////////
+// Ticket //
+////////////
+
+const $nameFieldTick = $('.product_cat-tickets .wccpf-fields-container').find('.first_name-wrapper, .last_name-wrapper')
+
+$nameFieldTick.each( (idx, el) => {
+	const $el = $(el)
+	const label = $el.find('.wccpf_label').children('label').text()
+	console.log(label)
+	$el.find('input').attr('placeholder', label.trim());
+})
 
 })
